d398d8f5ae823dfe6b8aa7a8fff4a7818cf99e04
Ilia Perfilov <ind.climb@gmail.com>
2023-08-06 23:38:03 +0300

    CoorseSerializer was updated and PaymentSerializer was created

--- lessons/serializers.py
+++ lessons/serializers.py
@@ -1,17 +1,29 @@
 from rest_framework import serializers
 
-from lessons.models import Course, Lesson
-
-
-class CourseSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Course
-        fields = '__all__'
+from lessons.models import Course, Lesson, Payment
 
 
 class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons = LessonSerializer(source='lesson', many=True, read_only=True)
+    number_of_lessons = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+    def get_number_of_lessons(self, instance):
+        return instance.lesson.all().count()
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
         fields = '__all__'
