add2bfae279c12f62b10109f605b02ac4ad0cba4
Ilia Perfilov <ind.climb@gmail.com>
2023-08-10 17:14:01 +0300

    views.py was updated

--- lessons/views.py
+++ lessons/views.py
@@ -1,18 +1,28 @@
+from django.core import exceptions
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from lessons.models import Course, Lesson, Payment
+
+from lessons.models import Course, Lesson, Payment, Subscription
+from lessons.pagination import MyPagination
 from lessons.permissions import IsModerator, IsOwner, IsNotModerator, IsOwnerOrModerator
-from lessons.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
+from lessons.serializers import CourseSerializer, LessonSerializer, PaymentSerializer, SubscriptionSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    pagination_class = MyPagination
+
+    def get(self, request):
+        queryset = Course.objects.all()
+        paginated_queryset = self.paginate_queryset(queryset)
+        serializer = CourseSerializer(paginated_queryset, many=True)
+        return self.get_paginated_response(serializer.data)
 
     def get_permissions(self):
 
@@ -36,6 +46,13 @@ class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
     permission_classes = [IsAuthenticated]
+    pagination_class = MyPagination
+
+    def get(self, request):
+        queryset = Lesson.objects.all()
+        paginated_queryset = self.paginate_queryset(queryset)
+        serializer = LessonSerializer(paginated_queryset, many=True)
+        return self.get_paginated_response(serializer.data)
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
@@ -67,6 +84,13 @@ class PaymentListAPIView(generics.ListAPIView):
     filterset_fields = ('paid_course', 'paid_lesson', 'payment_method')
     ordering_fields = ('payment_data',)
     permission_classes = [IsAuthenticated]
+    pagination_class = MyPagination
+
+    def get(self, request):
+        queryset = Payment.objects.all()
+        paginated_queryset = self.paginate_queryset(queryset)
+        serializer = PaymentSerializer(paginated_queryset, many=True)
+        return self.get_paginated_response(serializer.data)
 
 
 class PaymentRetrieveAPIView(generics.RetrieveAPIView):
@@ -85,3 +109,28 @@ class PaymentDestroyAPIView(generics.DestroyAPIView):
     queryset = Payment.objects.all()
     permission_classes = [IsAuthenticated]
 
+
+class SubscriptionCreateAPIView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, **kwargs):
+        serializer = SubscriptionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        course_id = int(request.get_full_path().split('/')[3])
+        try:
+            serializer.save(user=request.user, course=Course.objects.get(id=course_id))
+        except:
+            raise exceptions.ObjectDoesNotExist(f'Курс с id = {course_id} не существует')
+        return Response(status=status.HTTP_201_CREATED)
+
+
+class SubscriptionDestroyAPIView(generics.DestroyAPIView):
+    queryset = Subscription.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def perform_destroy(self, instance):
+        if instance.user == self.request.user or self.request.user.is_superuser:
+            instance.is_active = False
+            instance.save()
+        else:
+            raise Exception('Вы не можете удалить эту подписку')
