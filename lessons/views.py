42f73823c48210167ea182715f4878b887376979
Ilia Perfilov <ind.climb@gmail.com>
2023-08-17 13:44:30 +0300

    method 'update' for CourseViewSet was refactored

--- lessons/views.py
+++ lessons/views.py
@@ -13,6 +13,7 @@ from lessons.pagination import MyPagination
 from lessons.permissions import IsModerator, IsOwner, IsNotModerator, IsOwnerOrModerator
 from lessons.serializers import CourseSerializer, LessonSerializer, PaymentSerializer, SubscriptionSerializer
 from lessons.services import create_session, create_payment
+from lessons.tasks import send_mail_course_update
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -25,6 +26,20 @@ class CourseViewSet(viewsets.ModelViewSet):
         paginated_queryset = self.paginate_queryset(queryset)
         serializer = CourseSerializer(paginated_queryset, many=True)
         return self.get_paginated_response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+        send_mail_course_update.delay(instance.title, instance.pk)
+        return Response(serializer.data)
 
     def get_permissions(self):
 
